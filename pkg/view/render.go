97cfd5748695917dce7b6adfc30882ffd95e2036
Salvatore Gonda <salvatore.gonda@gmail.com>
2022-06-24 19:36:59 +0200

    Fix data arg and deleted data struct first

--- pkg/view/render.go
+++ pkg/view/render.go
@@ -3,8 +3,5 @@ package view
 import "net/http"
 
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
-	d := Data{
-		Data: data,
-	}
-	return v.Template.ExecuteTemplate(w, v.Bootstrap, d)
+	return v.Template.ExecuteTemplate(w, v.Bootstrap, data)
 }
