144859c765bc56b9f0d0a02b1755dfcf8139d31d
Salvatore Gonda <salvatore.gonda@gmail.com>
2022-06-25 11:23:40 +0200

    Changed to test receiver of type view for a kind of method overloading

--- pkg/view/render.go
+++ pkg/view/render.go
@@ -6,6 +6,21 @@ import (
 	"github.com/deemount/shobber-tpl/pkg/models"
 )
 
-func (v *View) Render(w http.ResponseWriter, d models.Data) error {
-	return v.Template.ExecuteTemplate(w, v.Bootstrap, d)
+type RenderInterface View
+type RenderData View
+type RenderNil View
+
+func (vi *RenderInterface) Render(w http.ResponseWriter, d interface{}) error {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return vi.Template.ExecuteTemplate(w, vi.Bootstrap, d)
+}
+
+func (vd *RenderData) Render(w http.ResponseWriter, d models.Data) error {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return vd.Template.ExecuteTemplate(w, vd.Bootstrap, d)
+}
+
+func (vn *RenderNil) Render(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return vn.Template.ExecuteTemplate(w, vn.Bootstrap, nil)
 }
